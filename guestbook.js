579014a6ae9d36f2a265ccd40b07a4df75db0580
sasakit <sasakit@jp.ibm.com>
2022-06-21 19:56:08 +0900

    add url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://4da3c77e.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
